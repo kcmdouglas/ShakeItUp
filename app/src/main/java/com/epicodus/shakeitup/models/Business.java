71fa0c532c5958eed402f3c8552cfd6f31f43adb
Illia Kuchko and Nathan Romike <ikuchko@gmail.com>
2016-04-11 16:23:54 -0700

    create methods to return arrays with random businesses

--- app/src/main/java/com/epicodus/shakeitup/models/Business.java
+++ app/src/main/java/com/epicodus/shakeitup/models/Business.java
@@ -1,5 +1,7 @@
 package com.epicodus.shakeitup.models;
 
+import android.util.Log;
+
 import com.epicodus.shakeitup.services.YelpService;
 import com.google.android.gms.maps.model.LatLng;
 
@@ -78,6 +80,49 @@ public class Business {
                 break;
             case YelpService.FUN: funList.add(business);
                 break;
+        }
+    }
+
+    public static ArrayList<Business> getRandomDinner() {
+        return getRandomBusinesses(dinnerList);
+    }
+
+    public static ArrayList<Business> getRandomDrink() {
+        return getRandomBusinesses(drinkList);
+    }
+
+    public static ArrayList<Business> getRandomFun() {
+        return getRandomBusinesses(funList);
+    }
+
+    private static ArrayList<Business> getRandomBusinesses(ArrayList<Business> arrayList) {
+        ArrayList<Business> randomBusinesses = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int randomIndex = -1;
+            boolean notRepeated = true;
+            while (notRepeated) {
+                notRepeated = false;
+                randomIndex = (int)((Math.random() * (getDinnerList().size() - 1)) + 1);
+                for(int y = 0; y < randomBusinesses.size(); y++) {
+                    if (arrayList.get(randomIndex).equals(randomBusinesses.get(y))) {
+                        notRepeated = true;
+                        break;
+                    }
+                }
+            }
+            randomBusinesses.add(arrayList.get(randomIndex));
+        }
+        return randomBusinesses;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Business)) {
+            return false;
+        } else {
+            Business otherObject = (Business) object;
+            return this.getName().equals(otherObject.getName()) && this.getPhone().equals(otherObject.getPhone()) && this.getImageUrl().equals(otherObject.getImageUrl());
         }
     }
 }
