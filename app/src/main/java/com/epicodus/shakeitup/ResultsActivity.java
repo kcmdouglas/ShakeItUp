17fbe46720cf9057a77ed4e92c194bd9dba803d4
Jeffrey Ruder and Midori Bowen <ruderjt@gmail.com>
2016-04-11 12:11:34 -0700

    Add runtime permission request

--- app/src/main/java/com/epicodus/shakeitup/ResultsActivity.java
+++ app/src/main/java/com/epicodus/shakeitup/ResultsActivity.java
@@ -1,6 +1,12 @@
 package com.epicodus.shakeitup;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -17,11 +23,15 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ResultsActivity extends AppCompatActivity implements OnMapReadyCallback {
-    @Bind(R.id.firstPlaceImageView) ImageView mFirstPlaceImageView;
-    @Bind(R.id.secondPlaceImageView) ImageView mSecondPlaceImageView;
-    @Bind(R.id.thirdPlaceImageView) ImageView mThirdPlaceImageView;
+    @Bind(R.id.firstPlaceImageView)
+    ImageView mFirstPlaceImageView;
+    @Bind(R.id.secondPlaceImageView)
+    ImageView mSecondPlaceImageView;
+    @Bind(R.id.thirdPlaceImageView)
+    ImageView mThirdPlaceImageView;
 
     private GoogleMap mMap;
+    private static final int PERMISSION_REQUEST = 411;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +53,49 @@ public class ResultsActivity extends AppCompatActivity implements OnMapReadyCall
         mMap = googleMap;
 
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Tacos"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10));
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            mMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(ResultsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                    try {
+                        mMap.setMyLocationEnabled(true);
+                    } catch (SecurityException se) {
+                        se.printStackTrace();
+                    }
+
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
