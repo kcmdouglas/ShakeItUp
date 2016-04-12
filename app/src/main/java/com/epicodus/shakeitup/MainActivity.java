8d678952c876d6b9564d2d659666622e036ee303
Jeffrey Ruder and Midori Bowen <ruderjt@gmail.com>
2016-04-12 10:45:14 -0700

    fully implement custom info windows.

--- app/src/main/java/com/epicodus/shakeitup/MainActivity.java
+++ app/src/main/java/com/epicodus/shakeitup/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, ChooserActivity.class);
+        Intent intent = new Intent(this, ResultsActivity.class);
         startActivity(intent);
 
     }
